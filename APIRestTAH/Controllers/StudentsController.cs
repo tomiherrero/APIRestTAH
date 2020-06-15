0f74353e64e72b83e5a5e24038373d3fc91a6ec0
tomiherrero <tomas.herrero@hotmail.es>
2020-06-15 01:25:07 -0300

    feat(APIRestTAH): add request get

---
+++ APIRestTAH/Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Contexts;
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIRestTAH.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : ControllerBase
+
+    {
+        private readonly StudentsContexts context; 
+
+        public StudentsController(StudentsContexts context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<StudentsModels>> Get()
+        {
+            return context.Student.ToList();
+        }
+    }
+}
